71fdbb7992c73afcde33df6cb51e669fb50acc27
Juan-Paul Naude <ju.naude@gmail.com>
2015-07-13 15:57:07 +0200

    Enemy spawning and spawn points

---
+++ Assets/EnemyMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour {
+
+	Transform target;
+	float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+	}
+}
--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -4,27 +4,40 @@ using System.Collections;
 public class EnemyMovement : MonoBehaviour {
 
 	public GameObject enemy;
+	public float spawntime = 3f;
+	public Transform[] spawnPoints;
 
-	Transform target;
-	float speed = 1;
+	//Transform target;
+	//float speed = 1f;
 
 	// Use this for initialization
 	void Start () {
+
+		InvokeRepeating ("spawn", spawntime, spawntime);
 		// Find player object
-		target = GameObject.Find ("Player").transform;
-		/*enemy = GameObject.Find ("Enemy");
+		//target = GameObject.Find ("Player").transform;
+		//enemy = GameObject.Find("Enemy");
+		/*enemy = GameObject.FindWithTag ("Enemy");
+		enemy = GameObject.Instantiate;
 
 		// Make enemies
 		for (int i = 0; i < 5; i++) {
+			Debug.Log("Dirkie has small penis");
 			Instantiate(enemy);
 
 		}*/
+	}
+
+	void spawn()
+	{
+		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+		Instantiate (enemy, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// Move enemy towards player
-		transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+		//transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
 
 	}
 }
