40091ddaebe7d0e565393c4c224a3be93442e14c
Daniel R Pearson <daniel.pearson@telkomsa.net>
2015-07-17 17:41:13 +0200

    Added Lightbulb Assets. Coded Spawn Points. Added GUI lightbulb counter. LightBulbManager.cs added. Added appropriate assets.
    The GUI code is buggy, and my code needs to be refactorred. Which I will
    do another day because my Monodeveloper is fucking out on me ^^

--- Assets/Scripts/Collectables/Batteries/BatteryCollection.cs
+++ Assets/Scripts/Collectables/Batteries/BatteryCollection.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+			
 public class BatteryCollection : MonoBehaviour {
 
 	private int batteryCount = 0;
-	//public Vector2[] positions;
+	private int lightbulbCount = 0;
+	//public Vector2[] positions;	
 
 	// Use this for initialization
 	void Start () {
@@ -20,17 +21,23 @@ public class BatteryCollection : MonoBehaviour {
 
 	//Increment battery count and remove battery from the game
 	void OnTriggerEnter2D(Collider2D other) { 
-		//if (other.tag == "BatteryCollider") { 
-			batteryCount += 1; 
-		    Destroy(other.transform.parent.gameObject); //destroys the battery sprite
-			Destroy(other.gameObject); //destroys the sprite's collider
-			Debug.Log("Player touched Battery");
-			Debug.Log (batteryCount);
+
+		if (other.tag == "BatteryCollider") { 
+			batteryCount += 1;
+		} else if (other.tag == "LightbulbCollider") { 
+			lightbulbCount += 1;
+		}
+
+	    Destroy(other.transform.parent.gameObject); //destroys the battery sprite
+		Destroy(other.gameObject); //destroys the sprite's collider
+		Debug.Log("Player touched Battery");
+		Debug.Log (batteryCount);
 
 		//} 
 	}
 
 	void OnGUI(){
 		GUI.Box (new Rect (0, 0, 100, 40), "Batteries: " + batteryCount);
+		GUI.Box (new Rect (0, 50, 100, 40), "Lightbulbs: " + lightbulbCount);
 	}
 }
