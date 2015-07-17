8c8a8b9f6a77f77e6a9a9c7709bd68d5f890d5ae
vanaardep <pdvanaarde@gmail.com>
2015-07-17 20:37:33 +0200

    Added some basic lights

---
+++ Assets/Scripts/TorchMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TorchMovement : MonoBehaviour {
+
+	Transform target;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//0.9 torch dist -0.001f torch height
+		transform.position = target.position + new Vector3 (0, 0.8f, -0.001f);
+	}
+}
