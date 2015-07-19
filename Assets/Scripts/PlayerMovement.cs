05474a5c89700e47d920c872bbabf05fbaf7ba4f
vanaardep <pdvanaarde@gmail.com>
2015-07-19 15:56:56 +0200

    Player moves based on mouse now

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,23 +3,30 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public float speed;
+	//public float speed;
 	public Rigidbody2D rb;
+
+	//Public Vars
+	public Camera camera;
+	public float speed;
+	
+	//Private Vars
+	private Vector3 mousePosition;
+	private Vector3 direction;
+	private float distanceFromObject;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+		camera = Camera.main;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(KeyCode.D)) {
-			transform.Translate(Vector2.right * speed);
-			//transform.position += Vector3.right * speed;
-			//rb.AddForce(Vector2.right * speed);
-			//rb.velocity = Vector2.right;
+		if(Input.GetKey(KeyCode.W)) {
+			transform.Translate(-Vector2.up * speed);
 		}
-		if(Input.GetKey(KeyCode.A)) {
+		/*if(Input.GetKey(KeyCode.A)) {
 			transform.Translate(-Vector2.right * speed);
 		}
 		if (Input.GetKey (KeyCode.W)) {
@@ -27,8 +34,27 @@ public class PlayerMovement : MonoBehaviour {
 		}
 		if (Input.GetKey (KeyCode.S)) {
 			transform.Translate (-Vector2.up * speed);
-		}
+		}*/
 	}
+
+	void FixedUpdate() {
+		//if (Input.GetButton("Fire2")){
+			
+			//Grab the current mouse position on the screen
+			mousePosition = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, Input.mousePosition.z - camera.transform.position.z));
+			
+			//Rotates toward the mouse
+			rb.transform.eulerAngles = new Vector3(0,0,Mathf.Atan2((mousePosition.y - transform.position.y), (mousePosition.x - transform.position.x))*Mathf.Rad2Deg - 270);
+			
+			//Judge the distance from the object and the mouse
+			//distanceFromObject = (Input.mousePosition - camera.WorldToScreenPoint(transform.position)).magnitude;
+			
+			//Move towards the mouse
+			//rigidbody.AddForce(direction * speed * distanceFromObject * Time.deltaTime);
+			
+		//}//End Move Towards If Case
+		
+	}//End Fire3 If case
 
 	void OnCollisionEnter2D(Collision2D coll)
 	{
--- Assets/Scripts/TorchMovement.cs
+++ Assets/Scripts/TorchMovement.cs
@@ -7,12 +7,12 @@ public class TorchMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		target = GameObject.Find ("Player").transform;
+		//target = GameObject.Find ("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//0.9 torch dist -0.001f torch height
-		transform.position = target.position + new Vector3 (0, 0.8f, -0.001f);
+		//transform.position = target.position + new Vector3 (0, 0.8f, -0.001f);
 	}
 }
