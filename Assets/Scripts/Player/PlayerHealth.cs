f8ab4b38afd32ece302bcfe262a501e09b89c9ab
Daniel R Pearson <daniel.pearson@telkomsa.net>
2015-07-20 23:06:32 +0200

    Refactored collectables into Item Collection, Base Car Health script added, Player Health script Added, Display of base health + Player
    All light sources changed to "important" to fix weird switch off effect.
    Causes performance issues in large quantities :/

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	private int playerHealth = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+        if(coll.gameObject.tag=="Enemy") {
+        	if(playerHealth != 0){
+    	        playerHealth -= 1;
+        	}
+        	else{
+        		//GameObject.Destroy("Player");
+        	}
+    	}
+	}
+
+	void OnGUI(){
+		GUI.Box (new Rect (0, 100, 100, 40), "Health: " + playerHealth);
+	}
+
+}
